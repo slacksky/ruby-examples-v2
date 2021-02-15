0740cd76b13105d2d39fae4e4d5b43d681e936ea
slacksky <slacksky@gmail.com>
2021-02-15 16:57:22 -0500

    added examples for loops

---
+++ cycles.rb
@@ -0,0 +1,28 @@
+##loops and cycle examples
+
+
+# while example
+#x = 1
+#while x < 5 do
+#    puts "hello #{x}"
+#    x+=1
+#end
+
+#x = 1
+#loop do
+#    puts "hello #{x}"
+#    break if x > 5
+#    x+=1
+#end
+
+#for i in 1..10 do
+#    puts "hello #{i}"
+#end
+
+#[1,2,3,4].each do 
+#    |x| puts "hello #{x}"
+#end
+
+#[1,2,3,4].each  {|x| puts "hello #{x}"} #single line version
+
+4.times {|x| puts "hello #{x}"} # integer method
