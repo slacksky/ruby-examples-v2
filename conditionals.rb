4caa7590cafaa59979803bc176caff7fec8f3a38
slacksky <slacksky@gmail.com>
2021-02-15 15:07:00 -0500

    conditionals scond examples with roles

--- conditionals.rb
+++ conditionals.rb
@@ -2,10 +2,23 @@
 
 ## contitionals examples is the user authenticated
 
-is_auth = true
+#is_auth = true
 
-if is_auth
+#if is_auth
+#    puts "admin page"
+#else
+#    puts "login page"
+#end
+
+
+#role = :admin
+#role = :superadmin
+role = :who?
+
+if role == :admin
     puts "admin page"
+elsif role == :superadmin
+    puts "superadmin page"
 else
     puts "login page"
 end
