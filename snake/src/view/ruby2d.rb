ebeee2167398069ef3e7f6e9cd131f79c06588bc
Jviv <slacksky@gmail.com>
2021-02-18 16:08:41 -0500

    render food based on the initial state

--- snake/src/view/ruby2d.rb
+++ snake/src/view/ruby2d.rb
@@ -14,9 +14,26 @@ module View
                 width: @pixel_size * state.grid.cols, 
                 height: @pixel_size * state.grid.rows
                 )
-            #render_snake
+            render_food(state)
+            render_snake(state)
 
             show
+        end
+
+        private
+        def render_food(state)
+            food = state.food
+            extend Ruby2D::DSL
+            Square.new(
+                x: food.col * @pixel_size, 
+                y: food.row * @pixel_size, 
+                size: @pixel_size,
+                color: 'yellow'
+              )
+
+        end
+        def render_snake(state)
+
         end
     end
 end
