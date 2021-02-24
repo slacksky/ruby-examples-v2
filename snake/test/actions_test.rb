414d8bbaadf7aafe82e745dca3d02b72bd900ad4
Jviv <slacksky@gmail.com>
2021-02-23 23:21:55 -0500

    stating to add the actions tests

---
+++ snake/test/actions_test.rb
@@ -0,0 +1,31 @@
+require "minitest/autorun"
+require_relative "../src/actions/actions.rb" #reference actions  and the instances avaiable 
+
+ 
+class ActionsTest < Minitest::Test
+    def test_move_snake
+        initial_state = Model::State.new(
+            Model::Snake.new([
+                Model::Coord.new(1,1),
+                Model::Coord.new(0,1)
+            ]),
+            Model::Food.new(4,4),
+            Model::Grid.new(8,12),
+            Direction::DOWN,
+            false 
+        )
+        expected_state = initial_state = Model::State.new(#confirm why the intial state is reassigned#
+            Model::Snake.new([
+                Model::Coord.new(2,1),
+                Model::Coord.new(1,1)
+            ]),
+            Model::Food.new(4,4),
+            Model::Grid.new(8,12),
+            Direction::DOWN,
+            false 
+        )
+
+        actual_state = Actions::move_snake(initial_state)
+        assert_equal actual_state, expected state
+    end
+end
